e5b03fa43366a3013b9a01639c79aaf1bf6c3d70
Alvaro Padilla <varoboss.ap@gmail.com>
2021-05-25 16:03:42 -0500

    Add auth service

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,17 +1,10 @@
-const db = require("../models");
-const { authSecret } = require("../config");
-const User = db.users;
-
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+const { AuthService } = require("../services/auth");
 
 exports.signup = async (req, res) => {
   try {
-    await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 8)
-    });
+    const newUser = req.body;
+    const { user } = await new AuthService().SignUp(newUser);
+
     res.send({ message: "User was registered successfully!" });
   } catch (error) {
     res.status(500).send({ message: error.message });
@@ -20,34 +13,10 @@ exports.signup = async (req, res) => {
 
 exports.signin = async (req, res) => {
   try {
-    const user = await User.findOne({ where: { username: req.body.username } });
+    const { username, password } = req.body;
+    const { status, response }= await new AuthService().SignIn({ username, password });
 
-    if (!user) {
-      return res.status(404).send({ message: "User Not found." });
-    }
-
-    const passwordIsValid = bcrypt.compareSync(
-      req.body.password,
-      user.password
-    );
-
-    if (!passwordIsValid) {
-      return res.status(401).send({
-        accessToken: null,
-        message: "Invalid Password!"
-      });
-    }
-
-    const token = jwt.sign({ id: user.id }, authSecret, {
-      expiresIn: 86400 // 24 hours
-    });
-
-    res.status(200).send({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      accessToken: token
-    });
+    res.status(status).send(response);
   } catch(error) {
     res.status(500).send({ message: error.message });
   }
